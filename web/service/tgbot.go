81932981ac3f15bdc31d96f688fa5287b3711fc1
浩天 <xeefei@gmail.com>
2025-12-10 14:17:44 +0800

    Update tgbot.go

--- web/service/tgbot.go
+++ web/service/tgbot.go
@@ -2077,13 +2077,16 @@ https://gomami.io/aff.php?aff=174
 4、ISIF优质亚太优化线路：
 https://cloud.isif.net/login?affiliation_code=333
 
-5、Bagevm优质落地鸡（原生IP全解锁）：
+5、ZoroCloud全球优质原生家宽&住宅双lSP，跨境首选：
+https://my.zorocloud.com/aff.php?aff=1072
+
+6、Bagevm优质落地鸡（原生IP全解锁）：
 https://www.bagevm.com/aff.php?aff=754
 
-6、白丝云【4837线路】实惠量大管饱：
+7、白丝云【4837线路】实惠量大管饱：
 https://cloudsilk.io/aff.php?aff=706
 
-7、RackNerd极致性价比机器：
+8、RackNerd极致性价比机器：
 https://my.racknerd.com/aff.php?aff=15268&pid=912`
 		// 〔中文注释〕: 发送消息时禁用链接预览，使界面更整洁
 		params := tu.Message(
